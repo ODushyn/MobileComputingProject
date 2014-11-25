dd145035122a0c60c0027024f6ca7cdc35303b54
Oleksandr <Oleksandr@Oleksandr-PC>
2014-11-25 02:24:56 +0200

    Constants added

---
+++ src/com/kpi/scheduler/tools/Constants.java
@@ -0,0 +1,20 @@
+package com.kpi.scheduler.tools;
+
+public class Constants {
+	public static int FIRST_SEMESTER = 1;
+	public static int SECOND_SEMESTER = 2;
+	
+	public static int OCCUR_EVERY_WEEK = 0;
+	public static int OCCUR_FIRST_WEEK = 1;
+	public static int OCCUR_SECOND_WEEK = 2;
+	
+	public static int FIRST_WEEK = 1;
+	public static int SECOND_WEEK = 2;
+	
+	public static int FIRST_LESSON = 1;
+	public static int SECOND_LESSON = 2;
+	public static int THIRD_LESSON = 3;
+	public static int FOURTH_LESSON = 4;
+	public static int FIRTHLESSON = 5;
+	public static int SIXTHLESSON = 6;
+}
